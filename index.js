a0f17b27b83ef2768a46c9408770340b87bdcf51
Marianne <marianneneuts@gmail.com>
2022-04-28 18:07:06 +0200

    feat: GET request test

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => res.send('Hello world!'));
+app.set('view engine', 'pug');
 
-app.listen(port, () => console.log('Example app listening on port ${port}'));
+app.get('/api/v1/messages', (req, res) => {
+    res.send("Hello world!");
+});
+
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}')
+});
