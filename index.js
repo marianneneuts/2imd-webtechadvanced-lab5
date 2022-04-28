1a1c2d3add38a5a83cf18d48455c609d4291a860
Marianne <marianneneuts@gmail.com>
2022-04-28 18:24:06 +0200

    feat: cors installation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const logger = require('./middleware/logger');
 const messagesRouter = require("./routes/messages");
+const cors = require('cors');
+
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 
 app.use(express.json());
+app.use(cors());
 app.use("/api/v1/messages", messagesRouter);
 
 app.get('/', (req, res) => {
