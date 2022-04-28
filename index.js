71a959dbfc81da9d9b38451fad93fa2a6562dd48
Marianne <marianneneuts@gmail.com>
2022-04-28 18:12:31 +0200

    feat: include middleware folder

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const logger = require('./middleware/logger');
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.set('view engine', 'pug');
 
-app.get('/api/v1/messages', (req, res) => {
+app.get('/api/v1/messages', logger, (req, res) => {
     res.send("Hello world!");
 });
 
---
+++ middleware/logger.js
@@ -0,0 +1,6 @@
+const logger = (req, res, next) => {
+    console.log("🚀");
+    next();
+}
+
+module.exports = logger;
